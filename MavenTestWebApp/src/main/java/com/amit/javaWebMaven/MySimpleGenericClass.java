6fc5ebec69b90d3db9a87e71b55cfde62c6d7795
asahu23 <asahu23@INCNU4289RDH.CSCMWS.CSCMWS.COM>
2020-01-03 11:25:25 -0500

    FIrst Commit.

---
+++ MavenTestWebApp/src/main/java/com/amit/javaWebMaven/MySimpleGenericClass.java
@@ -0,0 +1,9 @@
+package com.amit.javaWebMaven;
+
+import java.util.*;
+
+public class MySimpleGenericClass{
+	public static void main(String arr[]){
+		List<String> stringList = new ArrayList<String>();
+	}
+}
